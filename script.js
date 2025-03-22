fb8aa62a2869cb9d1de1403508fdc61d1c5f0f9c
renz2451 <166629964+renz2451@users.noreply.github.com>
2025-03-22 13:28:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.querySelectorAll(".download-button, .telegram-button");
-    
+
     buttons.forEach(button => {
         button.addEventListener("click", function (event) {
             event.preventDefault();
-            alert("This feature is not available yet!");
+            alert("Download link is not available yet!");
         });
     });
 });
