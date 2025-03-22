cfd913beaebb35cc4468c294507aca015f16d74d
renz2451 <166629964+renz2451@users.noreply.github.com>
2025-03-22 13:32:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll(".download-button, .telegram-button");
+    const buttons = document.querySelectorAll(".neon-button");
 
     buttons.forEach(button => {
         button.addEventListener("click", function (event) {
             event.preventDefault();
-            alert("Download link is not available yet!");
+            alert("Cyberpunk Feature Coming Soon!");
         });
     });
 });
